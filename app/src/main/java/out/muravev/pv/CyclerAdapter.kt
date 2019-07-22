7aaca821f5df10323b3017caa13e75fead022296
out-muravev-pv <pax27@mail.ru>
2019-07-22 11:22:52 +0300

    RecyclerView added

--- app/src/main/java/out/muravev/pv/CyclerAdapter.kt
+++ app/src/main/java/out/muravev/pv/CyclerAdapter.kt
@@ -5,23 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.cycler_view.view.*
+import kotlinx.android.synthetic.main.recycler_view.view.*
 
-class CyclerAdapter(val items: ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+class RecyclerAdapter(private var context: Context, private var dataList: ArrayList<String>) : RecyclerView.Adapter<ViewHolder>() {
     override fun getItemCount(): Int {
-        return items.size
+        return dataList.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int) : ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.cycler_view, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.recycler_view, parent, false))
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.stringsView?.text = items.get(position)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.stringsView.text = dataList.get(position)
     }
 
 }
 class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     val stringsView = view.strings_view
 }
-
--- app/src/main/java/out/muravev/pv/ResultActivity.kt
+++ app/src/main/java/out/muravev/pv/ResultActivity.kt
@@ -1,24 +1,22 @@
-//package out.muravev.pv
-//
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import kotlinx.android.synthetic.main.result_activity.*
-//
-//class ResultActivity : AppCompatActivity() {
-//
-//    lateinit var sortValUse: GlobalSortList
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.result_activity)
-//
-//        sortValUse = application as GlobalSortList
-//        result_text_view.text = "${sortValUse.sortedList}"
-//        sortValUse.listToFill.clear()
-//        button.setOnClickListener {
-//            finish()
-//        }
-//    }
-//
-//
-//}
+package out.muravev.pv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.result_activity.*
+
+class ResultActivity : AppCompatActivity() {
+
+    lateinit var sortValUse: GlobalSortList
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.result_activity)
+
+        sortValUse = application as GlobalSortList
+        result_text_view.text = "${sortValUse.sortedList}"
+        sortValUse.listToFill.clear()
+        button.setOnClickListener {
+            finish()
+        }
+    }
+}
